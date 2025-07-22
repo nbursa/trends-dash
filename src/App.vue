fc3e4288acd265e0a9191888345e4ddbf9fff637
Nenad <nbursa@gmail.com>
2025-07-22 19:48:29 +0200

    fix: cleanup root

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,5 @@ import Dashboard from '@/pages/DashboardPage.vue'
 </script>
 
 <template>
-  <main class="max-w-7xl mx-auto px-4 py-10 bg-background">
-    <Dashboard />
-  </main>
+  <Dashboard />
 </template>
