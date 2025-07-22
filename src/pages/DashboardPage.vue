a4591c120c9f3cd94368e0f29b8a826255e27bcf
Nenad <nbursa@gmail.com>
2025-07-22 11:45:48 +0200

    feat: init dashboard page

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="space-y-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div
+        v-for="metric in metrics"
+        :key="metric.key"
+        class="bg-background p-4 rounded-sm flex flex-col gap-1 border border-default"
+      >
+        <div class="text-sm">{{ metric.label }}</div>
+        <div class="text-xl font-semibold text-heading">{{ metric.value }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const metrics = [
+  { key: 'average', label: 'Average', value: 12 },
+  { key: 'top_3_position', label: 'Top 3 Position', value: 28.9 },
+  { key: 'top_3_percentage', label: 'Top 3 %', value: '68.5%' },
+  { key: 'market_share_position', label: 'Market Share Pos.', value: 15 },
+  { key: 'market_share_percentage', label: 'Market Share %', value: '72.4%' },
+]
+</script>
